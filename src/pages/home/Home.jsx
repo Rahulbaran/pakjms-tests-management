280cfa1891915ab9c1aa6697385f159f7b8534e3
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-06 20:28:30 +0530

    feat: add content to Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home-wrapper">
-      <h1>Homepage</h1>
+      <h1>आपका स्वागत है 🙏</h1>
+      <button className="btn btn-primary" onClick={() => navigate("/classes")}>
+        Go to Classes
+      </button>
     </div>
   );
 }
