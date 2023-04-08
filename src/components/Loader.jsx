12db4813d5e4e83f37c4679e87ebe74f42170083
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-08 18:11:40 +0530

    feat: create loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        <div className="loader-wheel"></div>
+        <h2 className="loader-text">Loading ...</h2>
+      </div>
+    </div>
+  );
+}
