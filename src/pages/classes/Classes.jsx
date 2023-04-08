a7586c8d9626a3f5cb7242c5166e05372e02cfbf
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-08 07:40:10 +0530

    feat: create Classes page component
    - provided links to pages of different classes
    - created an add new button for adding student

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router-dom";
+
 export default function Classes() {
   return (
     <div className="classes-wrapper">
-      <h1>Good Morning BIT</h1>
+      <div className="add-student-wrapper">
+        <button className="btn btn-primary">Add New Student</button>
+      </div>
+
+      <main className="classes-container">
+        <h1>All Classes</h1>
+        <div className="classes">
+          <Link to="1" className="btn btn-primary btn-link">
+            Class-1
+          </Link>
+          <Link to="2" className="btn btn-primary btn-link">
+            Class-2
+          </Link>
+          <Link to="3" className="btn btn-primary btn-link">
+            Class-3
+          </Link>
+          <Link to="4" className="btn btn-primary btn-link">
+            Class-4
+          </Link>
+          <Link to="5" className="btn btn-primary btn-link">
+            Class-5
+          </Link>
+          <Link to="6" className="btn btn-primary btn-link">
+            Class-6
+          </Link>
+          <Link to="7" className="btn btn-primary btn-link">
+            Class-7
+          </Link>
+          <Link to="8" className="btn btn-primary btn-link">
+            Class-8
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
