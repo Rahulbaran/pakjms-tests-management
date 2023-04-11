ba16305a3d836269b2fe77cfd97366c1ce0a9cc2
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-11 19:25:01 +0530

    feat: create a js file for fetching students
    - created fetchStudents.js file
    - used netlify function to fetch students from mongodb
    - students are fetched based on their classes

---
+++ functions/fetchStudents.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const [MONGO_USER, MONGO_PASSWORD] = [
+  process.env.MONGO_USER,
+  process.env.MONGO_PASSWORD
+];
+const mongoUrl = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@pakjms-apps.zywf02i.mongodb.net/?retryWrites=true&w=majority`;
+
+export const handler = async event => {
+  const classId = +event.queryStringParameters.classId;
+  const client = new MongoClient(mongoUrl);
+
+  try {
+    const db = client.db("tests-management");
+    const students = await db
+      .collection("students")
+      .find({ class_name: classId }, { sort: { student_name: 1 } })
+      .toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(students)
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  } finally {
+    await client.close();
+  }
+};
