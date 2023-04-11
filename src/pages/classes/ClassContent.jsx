c45e4a85c39ed7a0f9cea07b8601e8680cef3834
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-11 19:44:25 +0530

    ci: use TestModal in ClassContent component

--- src/pages/classes/ClassContent.jsx
+++ src/pages/classes/ClassContent.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
+/* Components */
+import TestModal from "../../components/class/modal/TestModal";
 import SubjectsTabs from "../../components/class/SubjectsTabs";
-import Tables from "../../components/class/Tables";
+import TestTables from "../../components/class/TestTables";
 
 export default function ClassContent({ subjects, classId }) {
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    // const dialog = document.querySelector(".dialog");
+
+    // Array.from(document.body.children).forEach(child => {
+    //   if (child !== dialog) child.inert = true;
+    // });
+
+    setModal(true);
+  };
+  const closeModal = () => setModal(false);
+
   return (
     <div className="class-content-wrapper">
+      <TestModal
+        classId={classId}
+        subjects={subjects}
+        modal={modal}
+        closeModal={closeModal}
+      />
       <SubjectsTabs subjects={subjects} />
-      <Tables classId={classId} />
+      <TestTables classId={classId} openModal={openModal} />
     </div>
   );
 }
