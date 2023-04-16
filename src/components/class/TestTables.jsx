0457ab69ed0f94a9303a6a978cc5d971d64e3c43
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-16 21:17:25 +0530

    ci: render tests in the UI
    - used netlify function to fetch "tests"
    - created refresh button for refreshing data
    - style refresh button
    - used util function for modifying date string

--- src/components/class/TestTables.jsx
+++ src/components/class/TestTables.jsx
@@ -1,51 +1,71 @@
-export default function Tables({ classId, openModal }) {
-  const arr = new Array(20).fill(Math.random());
+/* Components */
+import Loader from "../Loader";
 
+/* Utils */
+import modifyDateString from "../../utils/modifyDateString";
+
+export default function Tables({ classId, tests, openModal }) {
   return (
     <main className="tables-wrapper">
-      <button className="btn btn-primary" onClick={openModal}>
-        <span className="material-icons">add_circle</span>
-        <span>Add a test</span>
-      </button>
+      <div className="page-info">
+        <button className="btn btn-primary" title="refresh">
+          <span className="material-icons">refresh</span>
+        </button>
 
-      <h2>Class - {classId}</h2>
+        <h2>Class - {classId}</h2>
+
+        <button
+          className="btn btn-primary"
+          onClick={openModal}
+          title="Add a new test"
+        >
+          <span className="material-icons">add_circle</span>
+          <span>Test</span>
+        </button>
+      </div>
 
       <div className="tables">
-        <div className="table-container">
-          <div className="table-info">
-            <p>Test Date - 05/09/2024</p>
-            <div className="chart-container">
-              <button className="btn btn-primary">
-                <span className="material-icons">analytics</span>
-                <span>See Chart</span>
+        {tests.length === 0 ? (
+          <Loader />
+        ) : (
+          tests.map(test => (
+            <div className="table-container" key={test._id}>
+              <div className="table-info">
+                <p>Test Date - {modifyDateString(test.date)}</p>
+                <div className="chart-container">
+                  <button className="btn btn-primary">
+                    <span className="material-icons">analytics</span>
+                    <span>See Chart</span>
+                  </button>
+                </div>
+              </div>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th>SL. No.</th>
+                    <th>Students</th>
+                    <th>Marks ({test.fm})</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {test.students.map((student, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}.</td>
+                      <td>{student}</td>
+                      <td>{test.marks[index]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
+              <button className="btn btn-primary table-delete-btn">
+                <span className="material-icons">delete</span>
+                <span>Delete</span>
               </button>
             </div>
-          </div>
-
-          <table>
-            <thead>
-              <tr>
-                <th>SL. No.</th>
-                <th>Students</th>
-                <th>Marks</th>
-              </tr>
-            </thead>
-            <tbody>
-              {arr.map((ele, index) => (
-                <tr key={index}>
-                  <td>{index + 1}.</td>
-                  <td>Rahul Kumar - {String(ele).slice(0, 10)}</td>
-                  <td>45</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          <button className="btn btn-primary table-delete-btn">
-            <span className="material-icons">delete</span>
-            <span>Delete</span>
-          </button>
-        </div>
+          ))
+        )}
       </div>
     </main>
   );
