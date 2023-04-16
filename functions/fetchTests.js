088d93978af7b535b3ba82da2ccc0e0662b6a2c9
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-16 21:12:58 +0530

    feat: create netlify function for fetching tests from database
    - created fetchTests.js
    - it fetches the data from mongo database

---
+++ functions/fetchTests.js
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+
+const [MONGO_USER, MONGO_PASSWORD] = [
+  process.env.MONGO_USER,
+  process.env.MONGO_PASSWORD
+];
+const mongoUrl = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@pakjms-apps.zywf02i.mongodb.net/?retryWrites=true&w=majority`;
+
+export const handler = async event => {
+  const { classId, subject } = event.queryStringParameters;
+  const client = new MongoClient(mongoUrl);
+
+  try {
+    const db = client.db("tests-management");
+    const tests = await db
+      .collection(`class-${classId}`)
+      .find({}, { subject })
+      .toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tests)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
+  } finally {
+    await client.close();
+  }
+};
