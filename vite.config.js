d3a139e014b50769babbe4f606b0eef6aeb13966
Rahulkumar <rahulkumar109422@gmail.com>
2023-04-22 19:22:11 +0530

    chore: add vite configuration for using "vite-plugin-pwa" package(#11)

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 import lightningcss from "vite-plugin-lightningcss";
+import { VitePWA } from "vite-plugin-pwa";
+
+import manifest from "./manifest.json";
 
 export default defineConfig({
+  build: {
+    emptyOutDir: true
+  },
   plugins: [
     react(),
     legacy({
@@ -17,6 +23,22 @@ export default defineConfig({
       pseudoClasses: {
         focusVisible: "focus-visible"
       }
+    }),
+    VitePWA({
+      manifest,
+      manifestFilename: "site.webmanifest",
+      includeAssets: ["robots.txt"],
+      devOptions: {
+        enabled: false
+      },
+      workbox: {
+        globPatterns: [
+          "/**/*.{css,html,js}",
+          "/*.{ico,png,webp,gif,jpeg,avif}"
+        ],
+        sourcemap: true
+      },
+      registerType: "prompt"
     })
   ],
 
